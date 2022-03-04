62e64247d922b75aaf3d1c2c8fe6d8affe2f1a9a
Colton Plumley <cplumley1025@gmail.com>
2022-03-04 15:26:04 -0600

    Move ApolloProvider to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import './App.css';
-import { render } from 'react-dom';
 import LinkList from './components/LinkList';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
-});
 
 function App() {
   return (
@@ -15,12 +8,5 @@ function App() {
     </div>
   );
 }
-
-render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
 
 export default App;
